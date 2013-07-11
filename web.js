0509778a2a757379765da17adccbd37ea76ffb82
Ubuntu <ubuntu@ip-10-164-35-18.ec2.internal>
2013-07-11 03:15:22 +0000

    Add index.html

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buf = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var buf = fs.readFileSync("index.html");
   response.send(buf.toString());
 });
 
