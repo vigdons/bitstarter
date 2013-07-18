285a024d6149f1e82ed4530fd72501669a50070f
Ubuntu <ubuntu@ip-10-236-139-188.ec2.internal>
2013-07-18 05:04:03 +0000

    Incorporated Bootstrap

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
+  var buf = fs.readFileSync("index.html");
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
